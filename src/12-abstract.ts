586f6a1f0ddf8d1555fd14b50d9d984f1ea4b386
xMarioOP <marioalejandro424@outlook.com>
2024-10-24 15:05:34 -0500

    Abstract

---
+++ src/12-abstract.ts
@@ -0,0 +1,8 @@
+import { Animal, Dog } from "./09-protected";
+
+// const animal = new Animal("elite")  /// if Animal => abstract class Animal, we don´t create instances
+
+
+const cheis = new Dog("cheis", "nico")
+cheis.greeting()
+cheis.woof(2)
