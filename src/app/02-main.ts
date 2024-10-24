59544dba99c8853b027c8e330d95470629cb92bf
xMarioOP <marioalejandro424@outlook.com>
2024-10-24 16:46:38 -0500

    using product.service

---
+++ src/app/02-main.ts
@@ -0,0 +1,26 @@
+import { ProductMemoryService } from "./services/product.memory.service";
+
+const productService = new ProductMemoryService()
+
+
+productService.create(
+  {
+    title: "product1",
+    price: 100,
+    description: "a",
+    categoryId: 12,
+    images: []
+  }
+)
+
+const products = productService.getAll()
+
+const productId = products[0].id
+
+productService.updateProduct(productId, {
+  title: "aaa"
+})
+
+const rta = productService.findOne(productId)
+
+console.log(rta);
